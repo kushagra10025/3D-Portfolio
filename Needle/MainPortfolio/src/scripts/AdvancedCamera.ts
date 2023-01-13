de76643877f7060b1cc6283f2cc008e97aae2b43
Kushagra <kushagra.mckv@gmail.com>
2023-01-13 23:13:00 +0530

    KP
    - Changed Contact Form I Frame Overflow Mode to Visible due to Scroll Overflow Bug
    - Added pre made Camera Angles
    - Updated Transitions to now Work on gsap.Callback functionality
    - Integrated Transitions and Camera Angles
    - Updated Simple Transition Template

--- Needle/MainPortfolio/src/scripts/AdvancedCamera.ts
+++ Needle/MainPortfolio/src/scripts/AdvancedCamera.ts
@@ -42,10 +42,12 @@ export class AdvancedCamera extends Behaviour {
 	private _cameraObject !: Object3D;
 
 	private _transitions : any;
+	private _camAngles : any;
 
 	onEnable(): void {
 		this._controls = this.gameObject.getComponent(OrbitControls)!;
 		this._cameraObject = this.gameObject;
+		this.SetCamAngles();
 		this.SetTransitions();
 	}
 
@@ -58,6 +60,67 @@ export class AdvancedCamera extends Behaviour {
 	}
 
 	// Custom Private Functions
+	private SetCamAngles() {
+		if(this._controls.controls == null)
+			return;
+		const threeOrbit = this._controls.controls;
+		this._camAngles = {};
+
+		const convFact = (Math.PI/180);
+		const _defaultMinDistance = threeOrbit.minDistance;
+		const _defaultMaxDistance = threeOrbit.maxDistance;
+		const _defaultMinAzimuth = threeOrbit.minAzimuthAngle;
+		const _defaultMaxAzimuth = threeOrbit.maxAzimuthAngle;
+		const _defaultMinPolar = threeOrbit.minPolarAngle;
+		const _defaultMaxPolar = threeOrbit.maxPolarAngle;
+
+		// Default Angle
+		this._camAngles.resetAngle = () => {
+			threeOrbit.minDistance = _defaultMinDistance;
+			threeOrbit.maxDistance = _defaultMaxDistance;
+			threeOrbit.minAzimuthAngle = _defaultMinAzimuth;
+			threeOrbit.maxAzimuthAngle = _defaultMaxAzimuth;
+			threeOrbit.minPolarAngle = _defaultMinPolar;
+			threeOrbit.maxPolarAngle = _defaultMaxPolar;
+		};
+
+		// Direction Mast Cam Angle
+		this._camAngles.directionMastAngle = () => {
+			threeOrbit.minDistance = 3;
+			threeOrbit.maxDistance = 5;
+			threeOrbit.minAzimuthAngle = -45 * convFact;
+			threeOrbit.maxAzimuthAngle = -15 * convFact;
+			threeOrbit.minPolarAngle = 60 * convFact;
+			threeOrbit.maxPolarAngle = 120 * convFact;
+		};
+
+		// Blueprint Cam Angle
+		this._camAngles.blueprintAngle = () => {
+			threeOrbit.minDistance = 4;
+			threeOrbit.maxDistance = 17;
+			threeOrbit.minPolarAngle = 20 * convFact;
+			threeOrbit.maxPolarAngle = 90 * convFact;
+		};
+
+		// About & Projects Cam Angle
+		this._camAngles.aboutprojectsAngle = () => {
+			threeOrbit.minDistance = 0.75;
+			threeOrbit.maxDistance = 1.35;
+		};
+
+		// Interests Angle
+		this._camAngles.interestsAngle = () => {
+			threeOrbit.minDistance = 3.30;
+			threeOrbit.maxDistance = 3.75;
+		};
+
+		// Contact Angle
+		this._camAngles.contactAngle = () => {
+			threeOrbit.minDistance = 0.85;
+			threeOrbit.maxDistance = 1.15;
+		};
+	}
+
 	private SetTransitions() {
 		if(this._controls.controls == null)
 			return;
@@ -66,84 +129,129 @@ export class AdvancedCamera extends Behaviour {
 
 		// Direction Mast View
 		this._transitions.directionMastView = () => {
-			threeOrbit.enableZoom = false;
-			threeOrbit.enableRotate = false;
+			var lookOnStart = () => {
+				threeOrbit.enableZoom = false;
+				threeOrbit.enableRotate = false;
 
-			this.aboutProjectGoBackBtn.activeSelf = false;
-			this.interestsGoBackBtn.activeSelf = false;
-			this.contactViewGoBackBtn.activeSelf = false;
+				this.aboutProjectGoBackBtn.activeSelf = false;
+				this.interestsGoBackBtn.activeSelf = false;
+				this.contactViewGoBackBtn.activeSelf = false;
 
-			this.TranstionTemplate(1.5, this.directionMastLookAt, this.directionMastCamPos);
+				this._camAngles.resetAngle();
+			};
 
-			threeOrbit.enableZoom = true;
+			var lookOnComplete = () => {
+				threeOrbit.enableZoom = true;
+				threeOrbit.enableRotate = true;
+
+				this._camAngles.directionMastAngle();
+			};
+
+			this.TranstionTemplate(1.5, this.directionMastLookAt, this.directionMastCamPos, lookOnStart, lookOnComplete);
 		};
 
 		// Blueprint View
 		this._transitions.blueprintView = () => {
-			threeOrbit.enableZoom = false;
-			threeOrbit.enableRotate = false;
+			var lookOnStart = () => {
+				threeOrbit.enableZoom = false;
+				threeOrbit.enableRotate = false;
 
-			this.aboutProjectGoBackBtn.activeSelf = false;
-			this.interestsGoBackBtn.activeSelf = false;
-			this.contactViewGoBackBtn.activeSelf = false;
+				this.aboutProjectGoBackBtn.activeSelf = false;
+				this.interestsGoBackBtn.activeSelf = false;
+				this.contactViewGoBackBtn.activeSelf = false;
 
-			this.TranstionTemplate(1.5, this.blueprintViewLookAt, this.blueprintViewCamPos);
+				this._camAngles.resetAngle();
+			};
 
-			threeOrbit.enableZoom = true;
-			threeOrbit.enableRotate = true;
-			threeOrbit.autoRotate = true;
+			var lookOnComplete = () => {
+				threeOrbit.enableZoom = true;
+				threeOrbit.enableRotate = true;
+				threeOrbit.autoRotate = true;
+
+				this._camAngles.blueprintAngle();
+			};
+
+			this.TranstionTemplate(1.5, this.blueprintViewLookAt, this.blueprintViewCamPos, lookOnStart, lookOnComplete);
 		};
 
 		// About & Projects View
 		this._transitions.aboutprojectsView = () => {
-			threeOrbit.enableZoom = false;
-			threeOrbit.enableRotate = false;
+			var lookOnStart = () => {
+				threeOrbit.enableZoom = false;
+				threeOrbit.enableRotate = false;
 
-			this.TranstionTemplate(1.5, this.aboutProjectViewLookAt, this.aboutProjectViewCamPos);
+				this.aboutProjectGoBackBtn.activeSelf = false;
+				this.interestsGoBackBtn.activeSelf = false;
+				this.contactViewGoBackBtn.activeSelf = false;
 
-			this.aboutProjectGoBackBtn.activeSelf = true;
-			this.interestsGoBackBtn.activeSelf = false;
-			this.contactViewGoBackBtn.activeSelf = false;
+				this._camAngles.resetAngle();
+			};
 
-			threeOrbit.enableZoom = true;
+			var lookOnComplete = () => {
+				threeOrbit.enableZoom = true;
 
+				this.aboutProjectGoBackBtn.activeSelf = true;
+				this._camAngles.aboutprojectsAngle();
+			};
+
+			this.TranstionTemplate(1.5, this.aboutProjectViewLookAt, this.aboutProjectViewCamPos, lookOnStart, lookOnComplete);
 		};
 
 		// Interests View
 		this._transitions.interestsView = () => {
-			threeOrbit.enableZoom = false;
-			threeOrbit.enableRotate = false;
+			var lookOnStart = () => {
+				threeOrbit.enableZoom = false;
+				threeOrbit.enableRotate = false;
 
-			this.TranstionTemplate(1.5, this.interestsViewLookAt, this.interestsViewCamPos);
+				this.aboutProjectGoBackBtn.activeSelf = false;
+				this.interestsGoBackBtn.activeSelf = false;
+				this.contactViewGoBackBtn.activeSelf = false;
 
-			this.aboutProjectGoBackBtn.activeSelf = false;
-			this.interestsGoBackBtn.activeSelf = true;
-			this.contactViewGoBackBtn.activeSelf = false;
+				this._camAngles.resetAngle();
+			};
 
-			threeOrbit.enableZoom = true;
+			var lookOnComplete = () => {
+				threeOrbit.enableZoom = true;
+
+				this.interestsGoBackBtn.activeSelf = true;
+				this._camAngles.interestsAngle();
+			};
+
+			this.TranstionTemplate(1.5, this.interestsViewLookAt, this.interestsViewCamPos, lookOnStart, lookOnComplete);
 		}
 
 		// Contact View
 		this._transitions.contactsView = () => {
-			threeOrbit.enableZoom = false;
-			threeOrbit.enableRotate = false;
+			var lookOnStart = () => {
+				threeOrbit.enableZoom = false;
+				threeOrbit.enableRotate = false;
 
-			this.TranstionTemplate(1.5, this.contactViewLookAt, this.contactViewCamPos);
+				this.aboutProjectGoBackBtn.activeSelf = false;
+				this.interestsGoBackBtn.activeSelf = false;
+				this.contactViewGoBackBtn.activeSelf = false;
 
-			this.aboutProjectGoBackBtn.activeSelf = false;
-			this.interestsGoBackBtn.activeSelf = false;
-			this.contactViewGoBackBtn.activeSelf = true;
+				this._camAngles.resetAngle();
+			};
 
-			threeOrbit.enableZoom = true;
+			var lookOnComplete = () => {
+				threeOrbit.enableZoom = true;
+				
+				this.contactViewGoBackBtn.activeSelf = true;
+				this._camAngles.contactAngle();
+			}
+
+			this.TranstionTemplate(1.5, this.contactViewLookAt, this.contactViewCamPos, lookOnStart, lookOnComplete);
 		}
 	}
 
-	private TranstionTemplate(duration : number, lookAtVector : Vector3, cameraPositionVector : Vector3) {
+	private TranstionTemplate(duration : number, lookAtVector : Vector3, cameraPositionVector : Vector3, lookOnStart : gsap.Callback, lookOnComplete : gsap.Callback) {
 		gsap.to(this.lookAtObject.position, {
 			x : lookAtVector.x,
 			y : lookAtVector.y,
 			z : lookAtVector.z,
-			duration : duration
+			duration : duration,
+			onStart : lookOnStart,
+			onComplete : lookOnComplete
 		});
 
 		gsap.to(this._cameraObject.position, {
